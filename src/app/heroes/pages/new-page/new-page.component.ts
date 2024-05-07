a5e427762723a2e553cb5273efed981ee9ff400f
Javier Sanchez <javier.sanchez@vitaly.es>
2024-05-07 09:22:37 +0200

    App Hero

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class NewPageComponent {
-
+  public publishers = [
+    { id: 'DC comics', desc: 'CD - Comics' },
+    { id: 'Marvel comics', desc: 'Marvel - Comics' },
+  ];
 }
